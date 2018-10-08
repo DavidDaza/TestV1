2e16655396620b9f7d1f6c7579955b2680a718f6
PC-HOGAR\David <arcasd484@gmail.com>
2018-10-08 17:07:39 -0500

    proyecto web

---
+++ ClassLibrary1/LoginCls.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    internal class LoginCls
+    {
+        public LoginCls()
+        {
+        }
+
+        internal double autenticacionDeUsuarios(string v1, string v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ClassLibrary1/test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+namespace ClassLibrary1
+{
+    class test
+    {
+        [TestFixture]
+        public class LoginTestCls
+        {
+            [TestCase]
+            public void Autenticacion_de_usuario_correcta()
+            {
+                var login = new LoginCls();
+
+                Assert.AreEqual("Autenticacion de usuario correcta.", login.autenticacionDeUsuarios("uswe", "Pass1258/*"));
+
+            }
+
+        }
+    }
+}
