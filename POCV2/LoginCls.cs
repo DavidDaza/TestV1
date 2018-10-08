547d7817ae0ef08c2ce1240991de9b5ccc12946b
PC-HOGAR\David <arcasd484@gmail.com>
2018-10-08 11:08:58 -0500

    My new commit

---
+++ POCV2/LoginCls.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace POCV2
+{
+    internal class LoginCls
+    {
+        public LoginCls()
+        {
+        }
+
+        internal double autenticacionDeUsuarios(string v1, string v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
